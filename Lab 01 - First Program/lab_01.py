65e3594c135c295db83b98ed83e3267e37b23718
Vladimir <veseliykotrizhik@gmail.com>
2023-04-04 08:08:42 +0300

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -4,5 +4,9 @@ print('It was a long day')
 print('But it was perfect')
 
 print("I don't know how \nI don't why \nBut he did it")
-print("Today\tyou\tlooks\twonderful\tsweethart!")
+print("Today\tyou\tlooks\twonderful\tsweetheart!")
 print("I Hate You And Don't\rLove You!")
+
+print('''You can run,
+but you
+can't hide!''')
