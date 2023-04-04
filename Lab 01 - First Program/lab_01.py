afe439ac77b890853c0ac2c505b0ed097338133c
Vladimir <veseliykotrizhik@gmail.com>
2023-04-04 08:03:32 +0300

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,8 @@
-Hi
+print('Hello World')
+
+print('It was a long day')
+print('But it was perfect')
+
+print("I don't know how \nI don't why \nBut he did it")
+print("Today\tyou\tlooks\twonderful\tsweethart!")
+print("I Hate You And Don't\rLove You!")
